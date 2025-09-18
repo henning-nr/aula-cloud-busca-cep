b9fda5f2dc28dd00a1f4829914d4c7aa511a3c0c
Henning Summer <115693958+henning-nr@users.noreply.github.com>
2025-09-18 22:43:49 +0000

    ajustando buscca automativa dos logs em rua e cep

--- js/scripts/log/montarLogs.js
+++ js/scripts/log/montarLogs.js
@@ -4,13 +4,10 @@ function montarLogs(url) {
     console.log(params)
 
     if (params.length == 7) {
-        console.log("to no cep")
         document.querySelector("#cep-link").click()
         document.querySelector("#cep").value = params[4]
         buscaCepFetch()
-
     } else {
-        console.log("to na rua")
         document.querySelector("#rua-link").click()
         document.querySelector("#estado").value = params[4]
         document.querySelector("#rua").value = params[6]
